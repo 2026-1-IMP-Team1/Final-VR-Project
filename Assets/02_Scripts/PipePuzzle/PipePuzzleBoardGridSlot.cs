09f1e084c96c392f5f8ac2507327cbc416e1ff65
trueblue222 <shinieechul2012@naver.com>
2026-06-02 00:41:10 +0900

    PipePuzzle 기본 로직 구현

--- Assets/02_Scripts/PipePuzzle/PipePuzzleBoardGridSlot.cs
+++ Assets/02_Scripts/PipePuzzle/PipePuzzleBoardGridSlot.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 public class PipePuzzleBoardGridSlot : MonoBehaviour
 {
+    [Tooltip("그리드 좌표: (행, 열), 좌상단이 (0,0)")]
+    public Vector2Int gridPosition;
+
     public XRSocketInteractor socketInteractor;
     public bool IsOccupied => socketInteractor.hasSelection;
 
--- Assets/02_Scripts/PipePuzzle/PipePuzzlePiece.cs
+++ Assets/02_Scripts/PipePuzzle/PipePuzzlePiece.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using System.Collections.Generic;
 using System.Linq;
 
 public class PipePuzzlePiece : MonoBehaviour
 {
+    [Header("Pipe Directions")]
+    [SerializeField] private bool openUp;
+    [SerializeField] private bool openDown;
+    [SerializeField] private bool openLeft;
+    [SerializeField] private bool openRight;
+
     private XRGrabInteractable grabInteractable;
 
     private int rotationStep = 0;
@@ -88,4 +95,29 @@ public class PipePuzzlePiece : MonoBehaviour
     }
 
     public int RotationStep => rotationStep;
+
+    /// <summary>
+    /// 현재 회전 상태에서 실제로 열려 있는 방향 집합을 반환합니다.
+    /// rotationStep 1 = RotateLeft(CCW) 1회, rotationStep 3 = RotateRight(CW) 1회
+    /// </summary>
+    public HashSet<PipeDirection> GetOpenDirections()
+    {
+        var result = new HashSet<PipeDirection>();
+
+        // RotateRight(CW +90°Y)가 rotationStep을 -1(+3 mod4) 하므로
+        // CW 회전 횟수 = (4 - rotationStep) % 4
+        int cwCount = (4 - rotationStep) % 4;
+
+        if (openUp)    result.Add(RotateCW(PipeDirection.Up,    cwCount));
+        if (openRight) result.Add(RotateCW(PipeDirection.Right, cwCount));
+        if (openDown)  result.Add(RotateCW(PipeDirection.Down,  cwCount));
+        if (openLeft)  result.Add(RotateCW(PipeDirection.Left,  cwCount));
+
+        return result;
+    }
+
+    private static PipeDirection RotateCW(PipeDirection dir, int count)
+    {
+        return (PipeDirection)(((int)dir + count) % 4);
+    }
 }
